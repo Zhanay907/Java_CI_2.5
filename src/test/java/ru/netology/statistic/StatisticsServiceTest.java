1dad10dd8fea2770d8bf21655a098932948e6891
Zhanay Ilyassov <Zhanay86@gmail.com>
2021-04-05 18:29:12 +0600

    conter LINE

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatisticsServiceTest {
+
+  @Test
+  void findMax() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 14, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 14;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+}
